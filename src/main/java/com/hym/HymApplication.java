e382ab9dceb904a3cee0b258d5f4b6a890656f1c
lijunkou <420440960@qq.com>
2020-02-17 15:59:32 +0800

    修改

--- src/main/java/com/hym/HymApplication.java
+++ src/main/java/com/hym/HymApplication.java
@@ -11,6 +11,6 @@ public class HymApplication {
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(HymApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  HYM启动成功   ლ(´ڡ`ლ)ﾞ  \n" );
+        System.out.println("(♥◠‿◠)ﾉﾞ  HYM启动成功  !!!! ლ(´ڡ`ლ)ﾞ  \n" );
     }
 }
