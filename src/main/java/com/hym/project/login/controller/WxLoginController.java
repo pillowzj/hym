4439a493bd75d37317f6c50948f9e4ea136f4f96
lijunkou <420440960@qq.com>
2020-03-01 07:32:24 +0800

    update

--- src/main/java/com/hym/project/login/controller/WxLoginController.java
+++ src/main/java/com/hym/project/login/controller/WxLoginController.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.hym.common.constant.HttpStatus;
 import com.hym.common.utils.StringUtils;
+import com.hym.framework.domain.RequestData;
+import com.hym.framework.domain.ThreadCache;
 import com.hym.framework.redis.RedisCache;
 import com.hym.framework.web.domain.AjaxResult;
 import com.hym.project.login.WXLoginService;
@@ -24,15 +26,15 @@ public class WxLoginController {
 
     @Autowired
     private WXLoginService wxLoginService;
-    private Map res;
 
     @PostMapping("/loginByCellPhone")
-    public AjaxResult loginByCellPhone(String data) {
+    public AjaxResult loginByCellPhone() {
+        RequestData requestData = ThreadCache.getPostRequestParams();
+        JSONObject reqbody = JSON.parseObject(requestData.getData());
 
-        if (StringUtils.isEmpty(data)) {
+        if (StringUtils.isEmpty(reqbody)) {
             return AjaxResult.error();
         }
-        JSONObject reqbody = JSON.parseObject(data);
         String cellPhone = reqbody.getString("cellPhone");
         String verifyCode = reqbody.getString("verifyCode");
         //1 验证码验证
@@ -48,28 +50,35 @@ public class WxLoginController {
     }
 
     @PostMapping("/wxLogin")
-    public AjaxResult wxLogin(String data) {
-
-        if (StringUtils.isEmpty(data)) {
+    public AjaxResult wxLogin() {
+        RequestData requestData = ThreadCache.getPostRequestParams();
+        JSONObject reqbody = JSON.parseObject(requestData.getData());
+        if (StringUtils.isEmpty(reqbody)) {
             return AjaxResult.error();
         }
-        JSONObject reqbody = JSON.parseObject(data);
         String openid = reqbody.getString("openid");
         String name = reqbody.getString("name");
         String face = reqbody.getString("face");
+        String cellPhone = reqbody.getString("cellPhone");
+        String verifyCode = reqbody.getString("verifyCode");
         if (StringUtils.isEmpty(openid)) {
             return AjaxResult.error();
         }
+        //1 验证码验证
+        String vc = redisCache.getCacheObject("Constant.SMS_PREFIX" + cellPhone + verifyCode);
+        if (!vc.equals("Constant.SMS_PREFIX" + cellPhone + verifyCode)) {
+            return AjaxResult.error(HttpStatus.BAD_REPWD, " verify is error");
+        }
 
-        Map res = wxLoginService.wxLogin(openid, name, face);
+        Map res = wxLoginService.wxLogin(openid, name, face,cellPhone,verifyCode);
         System.out.println("login -->JSON.toJSON-->" + JSON.toJSON(res));
         return AjaxResult.success(res);
     }
 
     @PostMapping("/getOpenid")
-    public AjaxResult getOpenid(String data) {
-
-        JSONObject reqbody = JSON.parseObject(data);
+    public AjaxResult getOpenid() {
+        RequestData requestData = ThreadCache.getPostRequestParams();
+        JSONObject reqbody = JSON.parseObject(requestData.getData());
         String code = reqbody.getString("Code");
         WXOpenInfo res = wxLoginService.getWXInfo(code);
 
